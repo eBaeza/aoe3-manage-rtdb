4f418776ef9ffa73a235a50fc5544dff09dbb2bc
Edgar Aparicio Baeza <holamundo@edgarbaeza.mx>
2022-11-15 10:57:15 -0600

    Adding emulators config

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import { config as dotEnvConfig } from "dotenv-flow";
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { getDatabase, ref, remove, set, } from "firebase/database";
+import { getDatabase, ref, set, connectDatabaseEmulator } from "firebase/database";
 
 dotEnvConfig()
+
+const databaseURL = process.env.NODE_ENV === 'development'
+    ? 'http://127.0.0.1:9000/?ns=aoe3-companion'
+    : process.env.FB_DATABASE_URL
 
 const app = initializeApp({
     apiKey: process.env.FB_API_KEY,
     authDomain: process.env.FB_AUTH_DOMAIN,
+    databaseURL,
     projectId: process.env.FB_PROJECT_ID,
     storageBucket: process.env.FB_STORAGE_BUCKET,
     messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
@@ -17,31 +22,37 @@ const app = initializeApp({
 const auth = getAuth()
 const db = getDatabase(app);
 
+if (process.env.NODE_ENV === 'development') {
+    connectDatabaseEmulator(db, "localhost", 9000);
+}
+
 const langs = ['es', 'en', 'pt_br', 'zh', 'fr']
 
 async function uploadLanguages() {
-    await signInWithEmailAndPassword(auth, process.env.AUTH_EMAIL, process.env.AUTH_PASS)
+    try {
+        await signInWithEmailAndPassword(auth, process.env.AUTH_EMAIL, process.env.AUTH_PASS)
 
-    for (let lang of langs) {
-        console.log('===========================')
-        console.log(`1. Loading lang ${lang} json`)
-        const data = await import(`./data/localization/stringtabley_${lang}.json`, { assert: { type: "json" } })
-        console.log(`2. Indexing lang ${lang} json`)
-        const dataIndexed = data.default.stringtable.language.string.reduce((obj, { ['#text']: text, ...item }) => {
-            obj[item?.['@_locid']] = { ...item, text }
-            return obj
-        }, {})
-        const dbRef = ref(db, `localization/${lang}`)
-        console.log(`Removing lang ${lang} from DB`)
-        await remove(dbRef)
-        console.log(`Saving new values lang ${lang} to DB`)
-        await set(dbRef, dataIndexed)
-        console.log(`Saved lang ${lang} to DB`)
-        console.log('===========================\n\n')
+        for (let lang of langs) {
+            console.log('===========================')
+            console.log(`1. Loading lang ${lang} json`)
+            const data = await import(`./data/localization/stringtabley_${lang}.json`, { assert: { type: "json" } })
+            console.log(`2. Indexing lang ${lang} json`)
+            const dataIndexed = data.default.stringtable.language.string.reduce((obj, { ['#text']: text, ...item }) => {
+                obj[item?.['@_locid']] = { ...item, text, active: 0 }
+                return obj
+            }, {})
+            const dbRef = ref(db, `localization/${lang}`)
+            console.log(`Saving new values lang ${lang} to DB`)
+            await set(dbRef, dataIndexed)
+            console.log(`Saved lang ${lang} to DB`)
+            console.log('===========================\n\n')
+        }
+    } catch (error) {
+        console.error(err)
     }
 }
 
 uploadLanguages().then(() => {
     console.log('Languages upload ended')
     process.exit()
-}).catch(err => console.error(err))
+})
