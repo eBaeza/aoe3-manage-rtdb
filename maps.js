6ff5687b22f69c6ba08a45fe3ef4a72256337f29
Edgar Aparicio Baeza <holamundo@edgarbaeza.mx>
2023-03-06 23:38:58 -0600

    Version 1.41

---
+++ maps.js
@@ -0,0 +1,866 @@
+const maps = {
+    "afarabia": {
+        "id": 70
+    },
+    "afarabialarge": {
+        "id": 178
+    },
+    "afatlas": {
+        "id": 71
+    },
+    "afatlaslarge": {
+        "id": 96
+    },
+    "afcongobasin": {
+        "id": 72
+    },
+    "afcongobasinlarge": {
+        "id": 179
+    },
+    "afdarfur": {
+        "id": 73
+    },
+    "afdarfurlarge": {
+        "id": 74
+    },
+    "afdunes": {
+        "id": 75
+    },
+    "afduneslarge": {
+        "id": 97
+    },
+    "afgold coast": {
+        "id": 76
+    },
+    "afgold coastlarge": {
+        "id": 98
+    },
+    "afgreat rift": {
+        "id": 77
+    },
+    "afgreat riftlarge": {
+        "id": 99
+    },
+    "afhighlands": {
+        "id": 78
+    },
+    "afhighlandslarge": {
+        "id": 100
+    },
+    "afhorn": {
+        "id": 79
+    },
+    "afhornlarge": {
+        "id": 101
+    },
+    "afivorycoast": {
+        "id": 80
+    },
+    "afivorycoastlarge": {
+        "id": 102
+    },
+    "aflakechad": {
+        "id": 81
+    },
+    "aflakechadlarge": {
+        "id": 103
+    },
+    "aflakevictoria": {
+        "id": 82
+    },
+    "aflakevictorialarge": {
+        "id": 177
+    },
+    "aflostsahara": {
+        "id": 104
+    },
+    "aflostsaharalarge": {
+        "id": 105
+    },
+    "afniger delta": {
+        "id": 83
+    },
+    "afniger deltalarge": {
+        "id": 106
+    },
+    "afniger river": {
+        "id": 84
+    },
+    "afniger riverlarge": {
+        "id": 107
+    },
+    "afnile valley": {
+        "id": 85
+    },
+    "afnile valleylarge": {
+        "id": 86
+    },
+    "afpeppercoast": {
+        "id": 87
+    },
+    "afpeppercoastlarge": {
+        "id": 108
+    },
+    "afsahel": {
+        "id": 88
+    },
+    "afsahellarge": {
+        "id": 109
+    },
+    "afsavanna": {
+        "id": 89
+    },
+    "afsavannalarge": {
+        "id": 90
+    },
+    "afsiwaoasis": {
+        "id": 91
+    },
+    "afsiwaoasislarge": {
+        "id": 110
+    },
+    "afsudd": {
+        "id": 92
+    },
+    "afsuddlarge": {
+        "id": 111
+    },
+    "afswahilicoast": {
+        "id": 93
+    },
+    "afswahilicoastlarge": {
+        "id": 112
+    },
+    "aftassili": {
+        "id": 94
+    },
+    "aftassililarge": {
+        "id": 113
+    },
+    "aftranssahara": {
+        "id": 114
+    },
+    "aftranssaharalarge": {
+        "id": 115
+    },
+    "aftripolitania": {
+        "id": 95
+    },
+    "aftripolitanialarge": {
+        "id": 116
+    },
+    "alaska": {
+        "id": 49
+    },
+    "alaskalarge": {
+        "id": 117
+    },
+    "amazonia": {
+        "id": 1
+    },
+    "amazonialarge": {
+        "id": 118
+    },
+    "andes upper": {
+        "id": 47
+    },
+    "andes upperlarge": {
+        "id": 119
+    },
+    "andes": {
+        "id": 2
+    },
+    "andeslarge": {
+        "id": 120
+    },
+    "araucania": {
+        "id": 3
+    },
+    "araucanialarge": {
+        "id": 121
+    },
+    "arctic territories": {
+        "id": 50
+    },
+    "arctic territorieslarge": {
+        "id": 122
+    },
+    "bahia": {
+        "id": 123
+    },
+    "bahialarge": {
+        "id": 124
+    },
+    "baja california": {
+        "id": 51
+    },
+    "baja californialarge": {
+        "id": 125
+    },
+    "bayou": {
+        "id": 4
+    },
+    "bayoularge": {
+        "id": 126
+    },
+    "bengal": {
+        "id": 48
+    },
+    "bengallarge": {
+        "id": 127
+    },
+    "borneo": {
+        "id": 5
+    },
+    "borneolarge": {
+        "id": 128
+    },
+    "california": {
+        "id": 6
+    },
+    "californialarge": {
+        "id": 129
+    },
+    "caribbean": {
+        "id": 7
+    },
+    "caribbeanlarge": {
+        "id": 130
+    },
+    "carolina": {
+        "id": 8
+    },
+    "carolinalarge": {
+        "id": 18
+    },
+    "cascade range": {
+        "id": 52
+    },
+    "cascade rangelarge": {
+        "id": 131
+    },
+    "central plain": {
+        "id": 53
+    },
+    "central plainlarge": {
+        "id": 132
+    },
+    "centralasia": {
+        "id": 248
+    },
+    "centralasialarge": {
+        "id": 249
+    },
+    "ceylon": {
+        "id": 9
+    },
+    "ceylonlarge": {
+        "id": 133
+    },
+    "colorado": {
+        "id": 54
+    },
+    "coloradolarge": {
+        "id": 134
+    },
+    "dakota": {
+        "id": 55
+    },
+    "dakotalarge": {
+        "id": 135
+    },
+    "deccan": {
+        "id": 10
+    },
+    "deccanlarge": {
+        "id": 19
+    },
+    "euacropolis": {
+        "id": 250
+    },
+    "euacropolislarge": {
+        "id": 251
+    },
+    "eualps": {
+        "id": 180
+    },
+    "eualpslarge": {
+        "id": 181
+    },
+    "euanatolia": {
+        "id": 182
+    },
+    "euanatolialarge": {
+        "id": 183
+    },
+    "euarchipelago": {
+        "id": 184
+    },
+    "euarchipelagolarge": {
+        "id": 185
+    },
+    "euarena": {
+        "id": 262
+    },
+    "euarenalarge": {
+        "id": 263
+    },
+    "eubalkans": {
+        "id": 186
+    },
+    "eubalkanslarge": {
+        "id": 187
+    },
+    "eubaltic": {
+        "id": 188
+    },
+    "eubalticlarge": {
+        "id": 189
+    },
+    "eublackforest": {
+        "id": 190
+    },
+    "eublackforestlarge": {
+        "id": 191
+    },
+    "eubohemia": {
+        "id": 192
+    },
+    "eubohemialarge": {
+        "id": 193
+    },
+    "eubudapest": {
+        "id": 194
+    },
+    "eubudapestlarge": {
+        "id": 195
+    },
+    "eucarpathians": {
+        "id": 196
+    },
+    "eucarpathianslarge": {
+        "id": 197
+    },
+    "eucitizenclash": {
+        "id": 264
+    },
+    "eucitizenclashlarge": {
+        "id": 265
+    },
+    "eucourland": {
+        "id": 198
+    },
+    "eucourlandlarge": {
+        "id": 199
+    },
+    "eudanishstrait": {
+        "id": 200
+    },
+    "eudanishstraitlarge": {
+        "id": 201
+    },
+    "eudeluge": {
+        "id": 240
+    },
+    "eudnieperbasin": {
+        "id": 202
+    },
+    "eudnieperbasinlarge": {
+        "id": 203
+    },
+    "eueightyyearswar": {
+        "id": 241
+    },
+    "euengland": {
+        "id": 204
+    },
+    "euenglandlarge": {
+        "id": 205
+    },
+    "eufinland": {
+        "id": 206
+    },
+    "eufinlandlarge": {
+        "id": 207
+    },
+    "euforestnothing": {
+        "id": 266
+    },
+    "euforestnothinglarge": {
+        "id": 267
+    },
+    "eufrance": {
+        "id": 208
+    },
+    "eufrancelarge": {
+        "id": 209
+    },
+    "eugreatnorthernwar": {
+        "id": 242
+    },
+    "eugreatturkishwar": {
+        "id": 243
+    },
+    "euhungarianplains": {
+        "id": 210
+    },
+    "euhungarianplainslarge": {
+        "id": 211
+    },
+    "euiberia": {
+        "id": 212
+    },
+    "euiberialarge": {
+        "id": 213
+    },
+    "euireland": {
+        "id": 214
+    },
+    "euirelandlarge": {
+        "id": 215
+    },
+    "euitalianwars": {
+        "id": 244
+    },
+    "euitaly": {
+        "id": 216
+    },
+    "euitalylarge": {
+        "id": 217
+    },
+    "eukarelian": {
+        "id": 252
+    },
+    "eukarelianlarge": {
+        "id": 253
+    },
+    "eulithuania": {
+        "id": 218
+    },
+    "eulithuanialarge": {
+        "id": 219
+    },
+    "eulowcountries": {
+        "id": 220
+    },
+    "eulowcountrieslarge": {
+        "id": 221
+    },
+    "eumediterranean": {
+        "id": 268
+    },
+    "eumediterraneanlarge": {
+        "id": 269
+    },
+    "eunapoleonicwars": {
+        "id": 245
+    },
+    "eupomerania": {
+        "id": 254
+    },
+    "eupomeranialarge": {
+        "id": 255
+    },
+    "euportugal": {
+        "id": 256
+    },
+    "euportugallarge": {
+        "id": 257
+    },
+    "eupripetmarshes": {
+        "id": 222
+    },
+    "eupripetmarsheslarge": {
+        "id": 223
+    },
+    "eupyrenees": {
+        "id": 224
+    },
+    "eupyreneeslarge": {
+        "id": 225
+    },
+    "eurhine": {
+        "id": 258
+    },
+    "eurhinelarge": {
+        "id": 259
+    },
+    "eurussoturkwar": {
+        "id": 246
+    },
+    "eusardiniacorsica": {
+        "id": 226
+    },
+    "eusardiniacorsicalarge": {
+        "id": 227
+    },
+    "eusaxony": {
+        "id": 228
+    },
+    "eusaxonylarge": {
+        "id": 229
+    },
+    "euscandinavia": {
+        "id": 230
+    },
+    "euscandinavialarge": {
+        "id": 231
+    },
+    "euscotland": {
+        "id": 232
+    },
+    "euscotlandlarge": {
+        "id": 233
+    },
+    "eusteppe": {
+        "id": 234
+    },
+    "eusteppelarge": {
+        "id": 235
+    },
+    "eusteppelost": {
+        "id": 270
+    },
+    "eusteppelostlarge": {
+        "id": 271
+    },
+    "euthirtyyearswar": {
+        "id": 247
+    },
+    "euvistulabasin": {
+        "id": 236
+    },
+    "euvistulabasinlarge": {
+        "id": 237
+    },
+    "euwallachia": {
+        "id": 238
+    },
+    "euwallachialarge": {
+        "id": 239
+    },
+    "fertile crescent": {
+        "id": 56
+    },
+    "fertile crescentlarge": {
+        "id": 136
+    },
+    "florida": {
+        "id": 57
+    },
+    "floridalarge": {
+        "id": 137
+    },
+    "gran chaco": {
+        "id": 58
+    },
+    "gran chacolarge": {
+        "id": 138
+    },
+    "great lakes": {
+        "id": 11
+    },
+    "great lakeslarge": {
+        "id": 139
+    },
+    "great plains": {
+        "id": 12
+    },
+    "great plainslarge": {
+        "id": 20
+    },
+    "guianas": {
+        "id": 140
+    },
+    "guianaslarge": {
+        "id": 141
+    },
+    "himalayas": {
+        "id": 13
+    },
+    "himalayaslarge": {
+        "id": 142
+    },
+    "himalayasupper": {
+        "id": 14
+    },
+    "himalayasupperlarge": {
+        "id": 143
+    },
+    "hispaniola": {
+        "id": 15
+    },
+    "hispaniolalarge": {
+        "id": 144
+    },
+    "hokkaido": {
+        "id": 59
+    },
+    "hokkaidolarge": {
+        "id": 145
+    },
+    "honshu": {
+        "id": 16
+    },
+    "honshularge": {
+        "id": 146
+    },
+    "honshuregicide": {
+        "id": 36
+    },
+    "honshuregicidelarge": {
+        "id": 147
+    },
+    "indochina": {
+        "id": 17
+    },
+    "indochinalarge": {
+        "id": 148
+    },
+    "indonesia": {
+        "id": 60
+    },
+    "indonesialarge": {
+        "id": 149
+    },
+    "kamchatka": {
+        "id": 61
+    },
+    "kamchatkalarge": {
+        "id": 150
+    },
+    "korea": {
+        "id": 62
+    },
+    "korealarge": {
+        "id": 151
+    },
+    "malaysia": {
+        "id": 63
+    },
+    "malaysialarge": {
+        "id": 152
+    },
+    "manchuria": {
+        "id": 64
+    },
+    "manchurialarge": {
+        "id": 153
+    },
+    "mexico": {
+        "id": 65
+    },
+    "mexicolarge": {
+        "id": 154
+    },
+    "minasgerais": {
+        "id": 155
+    },
+    "minasgeraislarge": {
+        "id": 156
+    },
+    "mongolia": {
+        "id": 27
+    },
+    "mongolialarge": {
+        "id": 157
+    },
+    "new england": {
+        "id": 28
+    },
+    "new englandlarge": {
+        "id": 158
+    },
+    "northwest territory": {
+        "id": 29
+    },
+    "northwest territorylarge": {
+        "id": 159
+    },
+    "orinoco": {
+        "id": 30
+    },
+    "orinocolarge": {
+        "id": 160
+    },
+    "ozarks": {
+        "id": 31
+    },
+    "ozarkslarge": {
+        "id": 161
+    },
+    "painted desert": {
+        "id": 32
+    },
+    "painted desertlarge": {
+        "id": 162
+    },
+    "pampas sierras": {
+        "id": 66
+    },
+    "pampas sierraslarge": {
+        "id": 163
+    },
+    "pampas": {
+        "id": 33
+    },
+    "pampaslarge": {
+        "id": 164
+    },
+    "panama": {
+        "id": 165
+    },
+    "panamalarge": {
+        "id": 166
+    },
+    "parallel rivers": {
+        "id": 67
+    },
+    "parallel riverslarge": {
+        "id": 167
+    },
+    "patagonia": {
+        "id": 34
+    },
+    "patagonialarge": {
+        "id": 168
+    },
+    "plymouth": {
+        "id": 35
+    },
+    "plymouthlarge": {
+        "id": 169
+    },
+    "punjab": {
+        "id": 68
+    },
+    "punjablarge": {
+        "id": 170
+    },
+    "rockies": {
+        "id": 37
+    },
+    "rockieslarge": {
+        "id": 171
+    },
+    "saguenay": {
+        "id": 38
+    },
+    "saguenaylarge": {
+        "id": 21
+    },
+    "siberia": {
+        "id": 39
+    },
+    "siberialarge": {
+        "id": 22
+    },
+    "silkroad": {
+        "id": 40
+    },
+    "silkroadlarge": {
+        "id": 23
+    },
+    "sonora": {
+        "id": 41
+    },
+    "sonoralarge": {
+        "id": 24
+    },
+    "texas": {
+        "id": 42
+    },
+    "texasfrontier": {
+        "id": 172
+    },
+    "texasfrontierlarge": {
+        "id": 173
+    },
+    "texaslarge": {
+        "id": 25
+    },
+    "unknown": {
+        "id": 43
+    },
+    "unknownlarge": {
+        "id": 174
+    },
+    "unknownlost": {
+        "id": 272
+    },
+    "unknownlostlarge": {
+        "id": 273
+    },
+    "unknownsm": {
+        "id": 274
+    },
+    "unknownsmlarge": {
+        "id": 275
+    },
+    // "winterwonderland": {
+    //     "id": 69
+    // },
+    "yamal": {
+        "id": 260
+    },
+    "yamallarge": {
+        "id": 261
+    },
+    "yellow riverdry": {
+        "id": 44
+    },
+    "yellow riverlarge": {
+        "id": 26
+    },
+    "yucatan": {
+        "id": 45
+    },
+    "yucatanlarge": {
+        "id": 175
+    },
+    "yukon": {
+        "id": 46
+    },
+    "yukonlarge": {
+        "id": 176
+    },
+}
+
+import xml2js from 'xml2js';
+import fs from 'fs';
+const parser = new xml2js.Parser({ attrkey: 'attr' });
+const path = 'D:/SteamLibrary/steamapps/common/AoE3DE/Game/RandMaps/'
+
+const newMaps = {}
+
+Object.keys(maps).forEach((map) => {
+    let xml_string = fs.readFileSync(path + map + '.xml', "utf8");
+
+    parser.parseString(xml_string, function (error, result) {
+        if (error === null) {
+            if (result?.mapinfo) {
+                const { details, displayNameID, imagepath } = result?.mapinfo?.attr
+                newMaps[map] = {
+                    ...maps[map],
+                    details,
+                    displayNameID,
+                    imagepath,
+                }
+            }
+        }
+    });
+})
+
+fs.writeFile(
+    './myjsonfile.json',
+    JSON.stringify({
+        ...maps,
+        ...newMaps
+    }),
+    'utf8',
+    () => {
+        console.log('saved')
+    }
+);
+
+
